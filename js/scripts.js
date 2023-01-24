c1f2c1e5689fc1f10bfdb97b2679c220bfd63938
Diletta Pelloni <dilpel@live.it>
2023-01-24 14:48:04 +0100

    fatta milestone 1

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,28 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'bravo'
+            todolist: [
+                {
+                    text: 'fare la lavatrice',
+                    done: true
+                },
+                {
+                    text: 'stendere i panni',
+                    done: true
+                },
+                {
+                    text: 'stirare i panni',
+                    done: false
+                },
+                {
+                    text: 'fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'giocare a golf',
+                    done: false
+                }
+            ]
         }
     }
 }).mount('#app')
